f322d957475a0b3f7303907bf4cf8feee2266347
zackee12 <zack2190@gmail.com>
2014-07-27 15:26:42 -0700

    Add comments

--- Word-WritingTracker/Util.cs
+++ Word-WritingTracker/Util.cs
@@ -9,23 +9,46 @@ using Statistic = Microsoft.Office.Interop.Word.WdStatistic;
 namespace Word_WritingTracker
 {
     public static class Util
-    {
+    { 
+        /// <summary>
+        /// Gets the active document from the application
+        /// </summary>
+        /// <returns></returns>
         public static Word.Document GetActiveDocument() 
         {
             return Globals.ThisAddIn.Application.ActiveDocument;
         }
 
+        /// <summary>
+        /// Gets the specified statistic from a given word document
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="includeFootnotesAndEndnotes"></param>
+        /// <param name="statistic"></param>
+        /// <returns></returns>
         public static int GetStatistic(Word.Document document, bool includeFootnotesAndEndnotes, Statistic statistic)
         {
             object include = (object)includeFootnotesAndEndnotes;
             return document.ComputeStatistics(statistic, ref include);
         }
 
+        /// <summary>
+        /// Gets the word count from a specified document
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="includeFootnotesAndEndnotes"></param>
+        /// <returns></returns>
         public static int GetWordCount(Word.Document document, bool includeFootnotesAndEndnotes)
         {
             return GetStatistic(document, includeFootnotesAndEndnotes, Statistic.wdStatisticWords);
         }
 
+        /// <summary>
+        /// Gets the page count from a specified document
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="includeFootnotesAndEndnotes"></param>
+        /// <returns></returns>
         public static int GetPageCount(Word.Document document, bool includeFootnotesAndEndnotes)
         {
             return GetStatistic(document, includeFootnotesAndEndnotes, Statistic.wdStatisticPages);
