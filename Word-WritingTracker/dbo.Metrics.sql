d4a65e1f433965ec3624613c13ca941c6465578d
zackee12 <zack2190@gmail.com>
2014-07-27 18:31:49 -0700

    Add sql files

---
+++ Word-WritingTracker/dbo.Metrics.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Metrics] (
+    [ID]        INT        IDENTITY (1, 1) NOT NULL,
+    [FileID]    INT        NOT NULL,
+    [TimeStamp] DATETIME NOT NULL,
+    [WordCount] INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Metrics_ToTable] FOREIGN KEY ([FileID]) REFERENCES [dbo].[TrackedFiles] ([ID])
+);
+
---
+++ Word-WritingTracker/dbo.TrackedFiles.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TrackedFiles] (
+    [ID]          INT             IDENTITY (1, 1) NOT NULL,
+    [FileName]    NVARCHAR (MAX)  NOT NULL,
+    [Tracked]     BIT             DEFAULT 0 NOT NULL,
+    [ProjectName] NVARCHAR (1000) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_TrackedFiles_Column]
+    ON [dbo].[TrackedFiles]([ProjectName] ASC);
+
